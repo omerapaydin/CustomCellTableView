f643fb9158f76688cc6aef1093ff7e64420424a6
Ömer Apaydın <omerapaydin90@gmail.com>
2025-04-06 21:24:10 +0300

    Initial Commit

---
+++ CustomCellTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomCellTableView
+//
+//  Created by Ömer on 6.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CustomCellTableViewTests/CustomCellTableViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCellTableViewTests.swift
+//  CustomCellTableViewTests
+//
+//  Created by Ömer on 6.04.2025.
+//
+
+import Testing
+@testable import CustomCellTableView
+
+struct CustomCellTableViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
